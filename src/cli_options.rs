a1b76325bed415026b0ab2c279b7a0aa17be5a06
Jacob Rothstein <hi@jbr.me>
2020-09-06 14:53:33 -0700

    add better cli docs

--- src/cli_options.rs
+++ src/cli_options.rs
@@ -6,32 +6,74 @@ use tide_rustls::TlsListener;
 #[derive(StructOpt, Debug)]
 #[structopt(
     name = "tide-serve",
-    about = "a simple static http server built with tide"
+    about = "a simple static http server built with tide",
+    setting = structopt::clap::AppSettings::DeriveDisplayOrder
 )]
 pub struct CliOptions {
-    #[structopt(short = "o", long, env)]
-    host: Option<String>,
-
-    #[structopt(short, long, env, default_value = "8080")]
-    port: u16,
-
+    /// Filesystem path to serve
+    ///
+    /// Defaults to the current working directory
     #[structopt(parse(from_os_str), default_value)]
     root: RootPath,
 
+    /// Local host or ip to listen on
+    #[structopt(short = "o", long, env, default_value = "localhost")]
+    host: String,
+
+    /// Local port to listen on
+    #[structopt(short, long, env, default_value = "8080")]
+    port: u16,
+
+    /// Local listener spec to bind
+    ///
+    /// Examples:
+    ///     `--bind localhost:8080`
+    ///     `--bind http://localhost:8080`
+    ///     `--bind [::1]:1213`
+    ///
+    /// On unix-like systems only:
+    ///     `--bind http+unix:///var/run/some.socket`
+    ///     `--bind http+unix://./tmp/socket`
+    ///
+    /// --bind will override --host and --port.
     #[structopt(short, long, env = "TIDE_BIND")]
     bind: Option<String>,
 
+    /// Path to a tls certificate for tide_rustls
+    ///
+    /// This will be ignored unless key_path is also
+    /// provided. providing both key_path and cert_path enables tls.
+    ///
+    /// Example: `--cert ./cert.pem --key ./key.pem`
+    /// For development, try using mkcert
     #[structopt(short, long, env, parse(from_os_str))]
     cert_path: Option<PathBuf>,
 
+    /// The path to a tls key file for tide_rustls
+    ///
+    /// This will be ignored unless cert_path is also
+    /// provided. providing both key_path and cert_path enables tls.
+    ///
+    /// Example: `--cert ./cert.pem --key ./key.pem`
+    /// For development, try using mkcert
     #[structopt(short, long, env, parse(from_os_str))]
     key_path: Option<PathBuf>,
 
+    /// Host to forward (reverse proxy) not-found requests to
+    ///
+    /// This forwards any request that would otherwise be a 404 Not
+    /// Found to the specified listener spec.
+    ///
+    /// Examples:
+    ///    `--forward localhost:8081`
+    ///    `--forward http://localhost:8081`
+    ///    `--forward https://localhost:8081`
+    ///
+    /// Note: http+unix:// schemes are not yet supported
     #[structopt(short, long, env = "FORWARD")]
     forward: Option<String>,
 }
 
-const LOCALHOST: &str = "127.0.0.1";
 impl CliOptions {
     pub fn root(&self) -> &RootPath {
         &self.root
@@ -51,7 +93,7 @@ impl CliOptions {
                 ..
             } => DynListener::new(
                 TlsListener::build()
-                    .addrs((host.as_deref().unwrap_or(LOCALHOST), *port))
+                    .addrs((&host[..], *port))
                     .key(key)
                     .cert(cert),
             ),
@@ -60,9 +102,7 @@ impl CliOptions {
                 bind: Some(bind), ..
             } => DynListener::new(bind.clone()),
 
-            CliOptions { host, port, .. } => {
-                DynListener::new((host.as_deref().unwrap_or(LOCALHOST), *port))
-            }
+            CliOptions { host, port, .. } => DynListener::new((&host[..], *port)),
         }
     }
 }
