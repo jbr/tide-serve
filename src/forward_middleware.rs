cb0ae41ec37469745cdd1c4873aec226afb621c4
Jacob Rothstein <hi@jbr.me>
2020-09-06 19:01:15 -0700

    set forwarded header, use http-client directly

--- src/forward_middleware.rs
+++ src/forward_middleware.rs
@@ -1,7 +1,8 @@
+use http_client::{h1::H1Client, HttpClient};
 use std::net::ToSocketAddrs;
-use surf::Client;
+
 use tide::{
-    http::{self, Url},
+    http::{self, proxies::Forwarded, Url},
     Middleware, Next, Request, Result,
 };
 
@@ -22,6 +23,18 @@ impl ForwardMiddleware {
     }
 }
 
+fn forwarded(http_request: &http::Request) -> Forwarded<'static> {
+    let mut forwarded = Forwarded::from_headers(http_request.as_ref())
+        .ok()
+        .flatten()
+        .map(|f| f.into_owned())
+        .unwrap_or_else(|| Forwarded::new());
+    forwarded.add_for(http_request.peer_addr().unwrap_or("unknown"));
+    forwarded.set_by("_tide_serve");
+    forwarded.set_proto(http_request.url().scheme());
+    forwarded.into_owned()
+}
+
 #[tide::utils::async_trait]
 impl<T: Clone + Send + Sync + 'static> Middleware<T> for ForwardMiddleware {
     async fn handle(&self, mut request: Request<T>, next: Next<'_, T>) -> Result {
@@ -31,6 +44,9 @@ impl<T: Clone + Send + Sync + 'static> Middleware<T> for ForwardMiddleware {
         http_request.set_body(body);
         let response = next.run(request).await;
         if response.status().is_client_error() {
+            let forwarded = forwarded(&http_request);
+            forwarded.apply(&mut http_request);
+
             let url = http_request.url_mut();
             url.set_scheme(self.0.scheme())
                 .map_err(|_| http::format_err!("could not set scheme of url {}", url))?;
@@ -38,8 +54,8 @@ impl<T: Clone + Send + Sync + 'static> Middleware<T> for ForwardMiddleware {
                 .map_err(|_| http::format_err!("could not set host of url {}", url))?;
             url.set_port(self.0.port())
                 .map_err(|_| http::format_err!("could not set port of url {}", url))?;
-            let http_response: http::Response = Client::new().send(http_request).await?.into();
-            return Ok(http_response.into());
+
+            return Ok(H1Client::new().send(http_request).await?.into());
         }
 
         Ok(response)
