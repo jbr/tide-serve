9953f60a561924a55e24355c35e4fcd40ca4adfe
Jacob Rothstein <hi@jbr.me>
2020-09-06 17:00:59 -0700

    improve handling for other 4xx responses

--- src/forward_middleware.rs
+++ src/forward_middleware.rs
@@ -28,18 +28,16 @@ impl<T: Clone + Send + Sync + 'static> Middleware<T> for ForwardMiddleware {
         let http_request: &http::Request = request.as_ref();
         let mut http_request = http_request.clone();
         let response = next.run(request).await;
-        if let Some(error) = response.error() {
-            if error.status() == 404 {
-                let url = http_request.url_mut();
-                url.set_scheme(self.0.scheme())
-                    .map_err(|_| http::format_err!("could not set scheme of url {}", url))?;
-                url.set_host(self.0.host().map(|h| h.to_string()).as_deref())
-                    .map_err(|_| http::format_err!("could not set host of url {}", url))?;
-                url.set_port(self.0.port())
-                    .map_err(|_| http::format_err!("could not set port of url {}", url))?;
-                let http_response: http::Response = Client::new().send(http_request).await?.into();
-                return Ok(http_response.into());
-            }
+        if response.status().is_client_error() {
+            let url = http_request.url_mut();
+            url.set_scheme(self.0.scheme())
+                .map_err(|_| http::format_err!("could not set scheme of url {}", url))?;
+            url.set_host(self.0.host().map(|h| h.to_string()).as_deref())
+                .map_err(|_| http::format_err!("could not set host of url {}", url))?;
+            url.set_port(self.0.port())
+                .map_err(|_| http::format_err!("could not set port of url {}", url))?;
+            let http_response: http::Response = Client::new().send(http_request).await?.into();
+            return Ok(http_response.into());
         }
 
         Ok(response)
